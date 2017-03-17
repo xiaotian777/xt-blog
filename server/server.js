1bd31b30bdfe4328e3f69cedfd7a3c05d2c83283
xt <zxt5885577@hotmail.com>
2017-03-18 00:21:06 +0800

    Fix CROS issue

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ let app = express();
 
 app.use(bodyParser.json());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 /*
  * ===================================== 
  * Post Route
